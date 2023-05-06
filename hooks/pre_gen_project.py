f95474cd200b51abf86b52022d140a42c3027e17
atuan26 <natuann26@gmail.com>
2023-05-06 13:46:26 +0700

    initial commit

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,9 @@
+
+from collections import OrderedDict
+cookiecutter_string = '''{{ cookiecutter }}'''
+cookiecutter = eval(cookiecutter_string)
+
+
+project_slug = "{{ cookiecutter.project_slug }}"
+if hasattr(project_slug, "isidentifier"):
+    assert project_slug.isidentifier(), "'{}' project slug is not a valid Python identifier.".format(project_slug)
